934278172ce1cb6c0af989d9f2f64f2521667418
Gabriel <gabrielcostamoura1@gmail.com>
2020-06-07 19:33:56 -0300

    :art: adding basic routes

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "AAAAAAAA"
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    # shows form
+    return render_template('index.html')
+
+
+@app.route('/calculator', methods=['GET', 'POST'])
+def calculator():
+    return render_template('calculator.html')
+
+
+@app.route('/history', methods=['GET', 'POST'])
+def history():
+    return render_template('history.html')
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
