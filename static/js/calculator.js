cf3fed14fe7ebbedc8b733d55bdf8e107d702a25
root <root@DESKTOP-IU325IM.localdomain>
2020-10-01 12:48:26 -0300

    :recycle: Calculator basic javascript logic

---
+++ static/js/calculator.js
@@ -0,0 +1,55 @@
+// Pega o input da calculadora
+let input = document.querySelector(".input");
+// Pega os botões numéricos e de operações
+let buttons = document.querySelectorAll('[name="digit"], [name="operation"]');
+
+input.addEventListener("change", () => {}); // Faz com que o input seja atualizado quando o botão for clicado
+
+/* Percorre a lista de botões e adiciona
+um eventListener, para que quando qualquer um desses botões - [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, +, -, /, x] - for clicado o seu texto seja concatenado no input da calculadora
+*/
+buttons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    event.preventDefault();
+    let clickedButton = event.target.textContent;
+    console.log(clickedButton)
+    let operationText = (input.value += clickedButton)
+    console.log(operationText, clickedButton.value);
+  });
+});
+
+/**
+ * Botão que limpa o input da calculadora, essa função define o valor do input para uma 
+ * string vazia = ""
+ */
+let clearButton = document.querySelector('[name="clear"]').addEventListener('click', (event) => {
+  event.preventDefault();
+  input.value = "";
+})
+
+
+let calculateButton = document
+  .querySelector('[name="calculate"]')
+  .addEventListener("click", (event) => {
+    event.preventDefault()
+    let mathAccount = input.value
+    let splited = mathAccount.match(/[^\d()]+|[\d.]+/g)
+    console.log(splited)
+    
+    let data = {
+      numberOne: splited[0],
+      numberTwo: splited[2],
+      operator: splited[1],
+    }
+   // Enviar para o BECKEND
+    console.log(data)
+   
+    // fetch("http://localhost:5304/calculate", {
+    //   method: "POST",
+    //   body: {data: input.value},
+    // })
+    //   .then((response) => {
+    //     return response.json(); 
+    //   })
+    //   .then((data) => input.value = data.result)
+  });
